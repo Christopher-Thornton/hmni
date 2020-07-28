54705684fa3f0fda5d8ebfa9e2356e0cc71a3b41
Christopher-Thornton <46800625+Christopher-Thornton@users.noreply.github.com>
2020-07-28 13:20:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup_args = dict(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
   ],
 )
 

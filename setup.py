ca3fdfb1a3b85953f3cb2ccdacb9ecbbe78f85fa
Christopher-Thornton <46800625+Christopher-Thornton@users.noreply.github.com>
2020-08-26 22:44:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf8') as f:
 
 setup_args = dict(
     name='hmni',
-    version='0.1.5',
+    version='0.1.6',
     description='Fuzzy Name Matching with Machine Learning',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -16,7 +16,7 @@ setup_args = dict(
     author_email='christopher_thornton@outlook.com',
     keywords=['fuzzy-matching', 'natural-language-processing', 'nlp', 'machine-learning', 'data-science', 'python', 'artificial-intelligence', 'ai'],
     url='https://github.com/Christopher-Thornton/hmni',
-    download_url='https://github.com/Christopher-Thornton/hmni/archive/v0.1.5.zip',
+    download_url='https://github.com/Christopher-Thornton/hmni/archive/v0.1.6.zip',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -24,6 +24,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
   ],
 )
 
